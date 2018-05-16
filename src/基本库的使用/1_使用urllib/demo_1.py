20057f5b1e59de5a6f6e850a1c411101b9e1a0f1
dingmouren <naildingmouren@gmail.com>
2018-05-16 08:04:36 +0800

    HTTP.client.Response实例

--- src/基本库的使用/1_使用urllib/demo_1.py
+++ src/基本库的使用/1_使用urllib/demo_1.py
@@ -1,9 +1,20 @@
 '''
 urlopen()函数
+
+HTTPResponse实例包装来自服务器的HTTP响应
 '''
 
 import urllib.request
 
 response = urllib.request.urlopen('https://www.python.org')
 print(response.read().decode('utf-8')) # 输出网页的html的源代码
-print(type(response)) # 输出响应的类型，<class 'http.client.HTTPResponse'> 是HTTPResponse类型的对象
+print('类型:',type(response)) # 输出响应的类型，<class 'http.client.HTTPResponse'> 是HTTPResponse类型的对象
+print('headers:',response.getheaders()) # 输出响应的header的元组列表
+print('fileno:',response.fileno())    # 输出响应底层套接字的fileno
+print('msg:',response.msg)
+print('http协议版本:',response.version)
+print('响应状态码:',response.status)
+print('响应的原因短语:',response.reason)
+
+
+
