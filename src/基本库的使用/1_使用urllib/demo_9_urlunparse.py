b65919308fffb41fc5013983cfb89d792857082b
dingmouren <naildingmouren@gmail.com>
2018-05-18 07:37:13 +0800

    urlunparse构造URL

---
+++ src/基本库的使用/1_使用urllib/demo_9_urlunparse.py
@@ -0,0 +1,8 @@
+from urllib.parse import urlunparse
+
+'''
+urlunparse()函数接受一个长度为6的可迭代对象(长度必须是6)，构造URL
+'''
+data = ['http','www.baidu.com','index.html','user','id=6','comment']
+url = urlunparse(data)
+print(type(url),url,sep='\n')
