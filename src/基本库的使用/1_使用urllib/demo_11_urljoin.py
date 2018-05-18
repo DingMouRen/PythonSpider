bc9d504e72a5f36fef2f05b71d668db42e377d00
dingmouren <naildingmouren@gmail.com>
2018-05-18 08:15:36 +0800

    urljoin()函数补充，生成URL

---
+++ src/基本库的使用/1_使用urllib/demo_11_urljoin.py
@@ -0,0 +1,13 @@
+from urllib.parse import urljoin
+
+'''
+urljoin()函数可以实现链接的解析 拼合与生成，可以生成新的URL
+urljoin()有两个参数：第一个参数是个基础链接，第二个参数是新的链接。
+该方法会分析基础链接的scheme netloc path三部分内容，新连接对缺失的部分进行补充，
+如果基础链接中存在，会使用新的链接的内容部分，基础链接中的params query和fragment会被忽略掉
+'''
+
+url1 = 'http://www.baidu.com?id=6'
+url2 = 'https://www.cuiqingcai.com/index.html?name=jack#comment'
+result = urljoin(url1,url2)
+print(result)
