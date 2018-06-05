5f74d4949237beec9d6169acbb852586171b1ce2
dingmouren <naildingmouren@gmail.com>
2018-06-06 07:20:41 +0800

    match()函数

--- src/1_基本库的使用/3_正则表达式/demo_1_macth.py
+++ src/1_基本库的使用/3_正则表达式/demo_1_macth.py
@@ -1,9 +1,10 @@
 import re
-
+'''
+match()函数会从字符串的起始位置匹配正则表达式，如果匹配，就返回匹配成功的结果，如果不匹配，就返回None
+'''
 content = 'Hello 123 4567 World_This is a Regex Demo'
 print(len(content))
 result = re.match('^Hello\s\d\d\d\s\d{4}\s\w{10}',content)
 print(result)
 print(result.group())
-
 print(result.span())
