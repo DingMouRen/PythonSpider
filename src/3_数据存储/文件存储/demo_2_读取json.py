19d0dc6c6e4fa0dbc92b08cdc714497f7c9ba751
dingmouren <naildingmouren@gmail.com>
2018-06-10 10:39:11 +0800

    操作mysql

--- src/3_数据存储/文件存储/demo_2_读取json.py
+++ src/3_数据存储/文件存储/demo_2_读取json.py
@@ -4,8 +4,8 @@ db = pymysql.connect(host='localhost',user='root',password='root',port=3306,db='
 cursor = db.cursor()
 cursor.execute('SELECT VERSION()')
 # 1.查看数据库版本
-data = cursor.fetchone()
-print("数据库版本:",data)
+data1 = cursor.fetchone()
+print("数据库版本:",data1)
 # 2.创建新表
 cursor.execute('CREATE TABLE IF NOT EXISTS students (id VARCHAR(255) NOT NULL,name VARCHAR (255) NOT NULL ,age INT NOT NULL,PRIMARY KEY (id))')
 # 3.插入数据
@@ -18,21 +18,59 @@ try:
 except:
     db.rollback() # 这里加了一层异常处理，如果执行失败，rollback()执行数据回滚，相当于什么都没发生过，事务：原子性 一致性 隔离性 持久性
 # 4.插入数据的另一种写法
-data = {
+data4 = {
     'id':'200180909',
     'name':'jerry',
     'age':20
 }
-table = 'students'
-keys = ','.join(data.keys()) # join()方法将序列中的元素以指定的字符连接生成一个新的字符串。
-values = ','.join(['%s']*len(data))
-sql4 = 'INSERT INTO {table }({keys}) VALUES ({values })'.format(table=table,keys=keys,values=values)
+table4 = 'students'
+keys4 = ','.join(data4.keys()) # join()方法将序列中的元素以指定的字符连接生成一个新的字符串。
+values4 = ','.join(['%s']*len(data4))
+sql4 = 'INSERT INTO {table }({keys}) VALUES ({values })'.format(table=table4,keys=keys4,values=values4)
 try:
-    if cursor.execute(sql4,tuple(data.values())):
+    if cursor.execute(sql4,tuple(data4.values())):
         print("成功")
         db.commit()
 except:
     print("失败")
     db.rollback()
 # 5.更新数据
+data5 = {
+    'id':'200180909',
+    'name':'jerry',
+    'age':20
+}
+table5 = 'students'
+keys5 = ','.join(data5.keys()) # join()方法将序列中的元素以指定的字符连接生成一个新的字符串。
+values5 = ','.join(['%s']*len(data5))
+sql5 = 'INSERT INTO {table }({keys}) values ({values}) ON DUPLICATE KEY UPDATE'.format(table = table5,keys=keys5,values=values5)
+update = ','.join([" {key} = %s".format(key=key) for key in data5])
+sql5 += update
+try:
+    if cursor.execute(sql5,tuple(data5.values()*2)):
+        print('成功')
+        db.commit()
+except:
+    print('失败')
+    db.rollback()
+# 6.删除数据
+table6 = 'students'
+condition6 = 'age > 19'
+sql6 = 'DELETE FROM {table} WHERE {condition}'.format(table=table6,condition=condition6)
+try:
+    cursor.execute(sql6)
+    db.commit()
+except:
+    db.rollback()
+# 7.查询数据
+sql7 = 'SELECT * FROM students WHERE age >= 20'
+try:
+    cursor.execute(sql7)
+    print("Count:",cursor.rowcount) # cursor.rowcount()返回查询结果的条数
+    row = cursor.fetchone() #fetchont()返回一重元组，指针是偏移指针，这里每循环一次，指针就偏移一条数据，随用随取，简单高效
+    while row:
+        print('Row:',row)
+        row = cursor.fetchone()
+except:
+    print('Error')
 db.close()
