005a05e006a7611b9c8fa5f2ad0bc561b719cd3d
dingmouren <naildingmouren@gmail.com>
2018-06-14 08:15:10 +0800

    动态渲染网页 节点交互

--- src/5_动态渲染页面爬取/demo_4_节点交互.py
+++ src/5_动态渲染页面爬取/demo_4_节点交互.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 import time
+'''
+Chrome与chromedriver版本必须对应，否则无法交互
 
+'''
 browser = webdriver.Chrome()
-
 browser.get('https://www.taobao.com')
+input_search = browser.find_element_by_id('q')
+input_search.send_keys('iphone')
+time.sleep(1)
+input_search.clear()
+input_search.send_keys('荣耀play')
+button = browser.find_element_by_class_name('btn-search')
+button.click()
+browser.close()
