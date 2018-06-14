df51211fab7c84bd0575fe04b2f87d86763f5833
dingmouren <naildingmouren@gmail.com>
2018-06-14 08:30:12 +0800

    动态渲染网页 执行javascript

--- src/5_动态渲染页面爬取/demo_6_执行javascript.py
+++ src/5_动态渲染页面爬取/demo_6_执行javascript.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+'''
 
+执行JavaScript代码
+'''
 browser = webdriver.Chrome()
 browser.get('https://www.zhihu.com/explore')
 browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
